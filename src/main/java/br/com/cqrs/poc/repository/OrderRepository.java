c01d6ea1ab116d7329a79a131cedf8c2cf3d3d15
Leonardo Blume <leribeiro.blume@gmail.com>
2024-09-03 13:20:39 -0300

    chore: deleting thrash

--- src/main/java/br/com/cqrs/poc/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.cqrs.poc.repository;
-
-import br.com.cqrs.poc.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, String> {
-}
